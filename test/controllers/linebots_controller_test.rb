cb1b12395a29bfbcd9a49e9a85e29cb51e9139a4
toshiki-amii <80087083+toshiki-amii@users.noreply.github.com>
2021-05-02 11:06:46 +0900

    コントローラーとルーティングの修正

--- test/controllers/linebots_controller_test.rb
+++ test/controllers/linebots_controller_test.rb
@@ -1,6 +1,6 @@
 require 'test_helper'
 
-class LinebotControllerTest < ActionDispatch::IntegrationTest
+class LinebotsControllerTest < ActionDispatch::IntegrationTest
   # test "the truth" do
   #   assert true
   # end
