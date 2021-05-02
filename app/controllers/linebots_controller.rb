af13ce666897d32899f0d7a9e846c1a080ca3301
toshiki-amii <80087083+toshiki-amii@users.noreply.github.com>
2021-05-02 11:11:01 +0900

    アプリから送信したメッセージをRailsで受け取る

--- app/controllers/linebots_controller.rb
+++ app/controllers/linebots_controller.rb
@@ -2,15 +2,16 @@ class LinebotsController < ApplicationController
     protect_from_forgery except: [:callback]
   
     def callback
+      body = request.body.read
     end
   
     private
    
-       def client
-         @client ||= Line::Bot::Client.new { |config|
-           config.channel_secret = ENV["LINE_CHANNEL_SECRET"]
-           config.channel_token = ENV["LINE_CHANNEL_TOKEN"]
-         }
-       end
+    def client
+      @client ||= Line::Bot::Client.new { |config|
+        config.channel_secret = ENV["LINE_CHANNEL_SECRET"]
+        config.channel_token = ENV["LINE_CHANNEL_TOKEN"]
+      }
+    end
   
 end
