792409851f7669a2933a187656c120b6be9dd35b
toshiki-amii <80087083+toshiki-amii@users.noreply.github.com>
2021-05-03 11:06:10 +0900

    バブルコンテナの作成

--- app/controllers/linebots_controller.rb
+++ app/controllers/linebots_controller.rb
@@ -70,5 +70,14 @@ class LinebotsController < ApplicationController
           contents: bubbles
         }
       end
+
+      def set_bubble(hotel)
+        {
+          type: 'bubble',
+          hero: set_hero(hotel),
+          body: set_body(hotel),
+          footer: set_footer(hotel)
+        }
+      end
   
 end
